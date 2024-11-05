033f845f27cea445de9b1befdfac0e026430223b
Christina Hantakas <christinahantakas@Christinas-Air.attlocal.net>
2024-11-04 22:51:55 -0800

    Add routing to app.js with route for MoodHistory page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import MoodLogger from './MoodLogger';
-import MindfulnessQuote from './MindfulnessQuote'; 
-import MoodHistory from './MoodHistory';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import MoodLogger from './MoodLogger'; // Component for logging moods
+import MoodHistory from './MoodHistory'; // Component to see mood history
+import MindfulnessQuote from './MindfulnessQuote'; // Component for daily mindfulness quotes
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mood Tracker</h1>
+    <Router>
+      <div className="App">
+        <h1>Mood Tracker</h1>
+
+        {/* Navigation Bar */}
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/history">Mood History</Link>
+        </nav>
+
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<MainPage />} /> {/* Home route */}
+          <Route path="/history" element={<MoodHistory />} /> {/* Mood History page */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// Main page component w/ MindfulnessQuote and MoodLogger components
+function MainPage() {
+  return (
+    <div>
       <MindfulnessQuote /> 
-      <MoodLogger />
-      <MoodHistory />
+      <MoodLogger /> 
     </div>
   );
 }
