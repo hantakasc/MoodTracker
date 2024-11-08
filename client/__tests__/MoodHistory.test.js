13596121878bc076cf78cb9b3f7fd25d51687e66
Christina Hantakas <christinahantakas@Christinas-Air.attlocal.net>
2024-11-08 12:35:42 -0800

    Start test for MoodHistory component

---
+++ client/__tests__/MoodHistory.test.js
@@ -0,0 +1,13 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import MoodHistory from './MoodHistory';
+
+// Tests for the MoodHistory component
+describe('MoodHistory Component', () => {
+    test('renders mood history table with data', async () => {
+      
+    });
+  
+    test('handles delete button click', async () => {
+      
+    });
+  });
