caeeb137e408c2e3a0c54280b08e2193204f8a23
Christina Hantakas <christinahantakas@Christinas-Air.attlocal.net>
2024-11-11 22:00:57 -0800

    Import css and import privateroute to protect routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MoodLogger from './MoodLogger'; // Component for logging moods
 import MoodHistory from './MoodHistory'; // Component to see mood history
 import MindfulnessQuote from './MindfulnessQuote'; // Component for daily mindfulness quotes
+import LoginPage from './LoginPage'; // Component for login
+import PrivateRoute from './PrivateRoute'; // To protect routes
+import { useAuth0 } from '@auth0/auth0-react';
+import './App.css';
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+
   return (
     <Router>
       <div className="App">
@@ -14,24 +20,41 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/history">Mood History</Link>
+          
+          {isAuthenticated ? (
+            <>
+              <span>Hello, {user.name || user.email}!</span>
+              <button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
+            </>
+          ) : (
+            <button onClick={() => loginWithRedirect()}>Login</button>
+          )}
         </nav>
 
         {/* Define Routes */}
         <Routes>
-          <Route path="/" element={<MainPage />} /> {/* Home route */}
-          <Route path="/history" element={<MoodHistory />} /> {/* Mood History page */}
+          <Route path="/" element={<MainPage />} />
+          <Route
+            path="/history"
+            element={
+              <PrivateRoute>
+                <MoodHistory />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-// Main page component w/ MindfulnessQuote and MoodLogger components
+// Main page component w/ MindfulnessQuote and MoodLogger 
 function MainPage() {
   return (
     <div>
-      <MindfulnessQuote /> 
-      <MoodLogger /> 
+      <MindfulnessQuote />
+      <MoodLogger />
     </div>
   );
 }
